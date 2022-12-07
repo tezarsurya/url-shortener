c40e3b745b27b5e9d0b6f4d787d265dc30d07a43
tezarsurya <tzr.surya@gmail.com>
2022-12-07 12:07:55 +0700

    add nav links on large screens

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,38 @@
-import { lazy, Suspense } from "react";
+import logo from "../assets/logo.svg";
+interface NavlinkProps {
+  href: string;
+  text: string;
+}
 
-const Logo = lazy(() => import("./Logo"));
+const NavLink = ({ href, text }: NavlinkProps) => (
+  <a
+    href={href}
+    className="font-bold text-[#9e9aa7] transition-colors duration-150 ease-linear hover:text-[#35323e]"
+  >
+    {text}
+  </a>
+);
 
 const Navbar = () => {
   return (
-    <nav className="fixed top-0 z-50 flex w-full items-center justify-between border-b border-gray-100/50 bg-gray-100/30 px-6 pt-8 pb-4 shadow-lg shadow-gray-200/30 backdrop-blur-xl md:px-10 lg:px-28 xl:px-40">
+    <nav className="fixed top-0 z-50 flex w-full items-center justify-between border-b border-gray-100/50 bg-white/60 px-6 pt-8 pb-4 shadow-lg shadow-gray-200/30 backdrop-blur-xl md:px-10 lg:space-x-8 lg:px-28 xl:px-40">
       <a href="/">
-        <Suspense>
-          <Logo />
-        </Suspense>
+        <img src={logo} width={121} height={33} alt="Shortly Logo" />
       </a>
+      <div className="hidden flex-1 items-center justify-start space-x-6 lg:flex">
+        <NavLink href="#features" text="Features" />
+        <NavLink href="#" text="Pricing" />
+        <NavLink href="#" text="Resources" />
+      </div>
+      <div className="hidden items-center justify-end space-x-6 lg:flex">
+        <NavLink href="#" text="Login" />
+        <a
+          href="#"
+          className="rounded-full bg-[#2acfcf] px-5 py-2 font-bold text-white transition-colors duration-150 ease-linear hover:bg-[rgba(42,207,207,80%)]"
+        >
+          Sign Up
+        </a>
+      </div>
       <button className="lg:hidden">
         <svg
           xmlns="http://www.w3.org/2000/svg"
