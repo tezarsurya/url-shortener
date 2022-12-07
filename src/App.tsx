c58f26cc61959fa214e4918376c12273f658c66a
tezarsurya <tzr.surya@gmail.com>
2022-12-07 16:12:15 +0700

    add Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Boost from "./components/Boost";
 import Features from "./components/Features";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
@@ -10,11 +11,12 @@ function App() {
   return (
     <>
       <Navbar />
-      <main className="min-h-screen w-full pt-24">
+      <main className="min-h-screen w-full pt-12">
         <Hero />
         <Features />
         <Boost />
       </main>
+      <Footer />
     </>
   );
 }
